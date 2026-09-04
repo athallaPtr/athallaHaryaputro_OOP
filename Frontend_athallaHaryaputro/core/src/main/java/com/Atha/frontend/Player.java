04232576b797dac87c872ace51166a1948e5af6f
Athallah Haryaputro <Athallaharyaputro@gmail.com>
2026-09-04 14:53:16 +0700

    Resubmit cs module 1

--- Frontend_athallaHaryaputro/core/src/main/java/com/Atha/frontend/Player.java
+++ Frontend_athallaHaryaputro/core/src/main/java/com/Atha/frontend/Player.java
@@ -12,4 +12,30 @@ public class Player {
         this.power = power;
         this.spellCards = spellCards;
     }
+
+    public void takeDamage(int damage) {
+        hp -= damage;
+
+        if (hp < 0){
+            hp = 0;
+        }
+
+        if (hp > 0){
+            System.out.println(name + "took " + damage + " damage ! remaining HP: " + hp);
+        } else {
+            System.out.println(name + " was defeated !");
+        }
+    }
+
+    public void shoot(Enemy target) {
+        int damage = 10 + power;
+
+        System.out.println(name + "shoots " + target.name + " dealing " + damage + " DMG!");
+
+        targe.takeDamage(damage);
+    }
+
+    public boolean isAlive() {
+        return hp > 0;
+    }
 }
