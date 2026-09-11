8d212c45c544387616836200bfcec54d97a45fc6
Athallah Haryaputro <Athallaharyaputro@gmail.com>
2026-09-11 10:44:26 +0700

    selesai cs modul 2

--- Frontend_athallaHaryaputro/core/src/main/java/com/Atha/frontend/Enemy.java
+++ Frontend_athallaHaryaputro/core/src/main/java/com/Atha/frontend/Enemy.java
@@ -1,33 +1,56 @@
 package com.Atha.frontend;
 
-public class Enemy {
-    String name;
-    int hp;
-    int maxHp;
+import com.badlogic.gdx.graphics.Color;
+
+public class Enemy extends GameObject {
+    protected String name;
+    protected int hp;
+    protected int maxHp;
+    protected long scoreValue;
 
     public Enemy(String name, int hp) {
+        super(200, 380, 24, 24, 0, Color.PINK);
         this.name = name;
         this.hp = hp;
         this.maxHp = hp;
+        this.scoreValue = 100L;
     }
 
-    public void takeDamage(int damage) {
+    public Enemy(float x, float y, float width, float height, Color color, String name, int hp, long scoreValue) {
+        super(x, y, width, height, 0, color);
+        this.name = name;
+        this.hp = hp;
+        this.maxHp = hp;
+        this.scoreValue = scoreValue;
+    }
+
+    public boolean takeDamage(int damage) {
+        if (this.hp <= 0) {
+            return false;
+        }
+
         this.hp -= damage;
-        if (this.hp < 0) {
+        if (this.hp <= 0) {
             this.hp = 0;
-        }
-        System.out.println(name + " took " + damage + " damage! HP: " + this.hp + "/" + this.maxHp);
-        if (this.hp == 0) {
+            System.out.println(name + " took " + damage + " damage! HP: " + this.hp + "/" + this.maxHp);
             System.out.println(name + " was defeated!");
+            return true;
         }
+
+        System.out.println(name + " took " + damage + " damage! HP: " + this.hp + "/" + this.maxHp);
+        return false;
     }
 
     public void attack(Player player, int damage) {
-        System.out.println(name + " unleashes bullet barrage on " + player.name + "!");
+        System.out.println(name + " unleashes bullet barrage on " + player.getName() + "!");
         player.takeDamage(damage);
     }
 
     public boolean isAlive() {
         return this.hp > 0;
     }
-}
+
+    public String getName() {return name;}
+    public void setName(String name) { this.name = name: }
+
+public int getHp()  -
--- Frontend_athallaHaryaputro/core/src/main/java/com/Atha/frontend/Player.java
+++ Frontend_athallaHaryaputro/core/src/main/java/com/Atha/frontend/Player.java
@@ -1,21 +1,51 @@
 package com.Atha.frontend;
 
-public class Player {
-    String name;
-    int hp;
-    int power;
-    int spellCards;
+import com.badlogic.gdx.graphics.Color;
+
+public class Player extends GameObject {
+
+    private String name;
+    private int hp;
+    private int power;
+    private int spellCards;
+    private long score;
 
     public Player(String name, int hp, int power, int spellCards) {
+        super(280, 40, 32, 32, 0, Color.RED);
         this.name = name;
         this.hp = hp;
         this.power = power;
         this.spellCards = spellCards;
+        this.score = 0;
     }
+
+    public Player(float x, float y, String name, int hp, int power, int spellCards) {
+        super(x, y, 32, 32, 0, Color.RED);
+        this.name = name;
+        this.hp = hp;
+        this.power = power;
+        this.spellCards = spellCards;
+        this.score = 0;
+    }
+
+    public void addScore(long points) {
+        if (points > 0) {
+            this.score += points;
+        }
+    }
+
+    public void collectItem(Item item){
+        System.out.println(getName() + " collected " + item.getItemType() + "!");
+        if (item.getScoreValue() > 0){
+            addScore(item.getScoreValue());
+        }
+    }
+
+
 
     public void shoot(Enemy target) {
         int damage = 10 + power;
-        System.out.println(name + " shoots " + target.name + " dealing " + damage + " DMG!");
+        System.out.println(name + " shoots " + target.getName() + " dealing " + damage + " DMG!");
         target.takeDamage(damage);
     }
 
@@ -33,4 +63,21 @@ public class Player {
     public boolean isAlive() {
         return this.hp > 0;
     }
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) { this.name = name; }
+
+    public int getHp() {return hp: }
+    public void setHp(int hp) { this.hp = Math.max(0, hp); }
+
+    public int getPower() { return power: }
+    public void setPower(int power) { this.power = power; }
+
+    public int getSpellCards() { return spellCards: }
+    public void setSpellCards(int spellCards) {this.spellCards + spellCards; }
+
+    public long getScore() { return score; }
+
 }
